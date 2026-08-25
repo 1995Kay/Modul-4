051d3056a5690b3bb7db3d22ea8d89e3f4969ad1
1995Kay <kay-hansen@outlook.de>
2026-08-25 13:32:18 +0200

    Objekte Modul 4

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-let liste = [5, 9, 8, 3];
+let students = [];
 
-function add() {
-  const value = Number(document.getElementById("content").value);
-  liste.push(value);
-}
-function printAll() {
-  console.log(liste);
-  liste.forEach((element) => {
-    console.log(element);
-  });
-}
-function deleteByPosition() {
-  const Position = Number(document.getElementById("deletePosition").value);
-  if (Position < liste.length) {
-    liste.splice(Position, 1);
-  } else {
-    console.log(value);
-  }
-}
-function deleteByContent() {
-  const content = Number(document.getElementById("deleteContent").value);
-  const newListe = liste.filter((element) => {
-    return element !== content;
-  });
-  console.log(newListe);
-}
-function multiply() {
-  const newListe = liste.map((element) => {
-    return element * 2;
-  });
-  liste = newListe;
+function createStudent() {
+  // 1. Alle Werte aus den HTML-Inputs auslesen
+  const firstNameValue = document.getElementById("firstName").value;
+  const lastNameValue = document.getElementById("lastName").value;
+
+  // Wichtig: Zahlen immer mit Number() umwandeln!
+  const ageValue = Number(document.getElementById("age").value);
+  const classValue = Number(document.getElementById("schoolClass").value);
+
+  const englishValue = Number(document.getElementById("english").value);
+  const germanValue = Number(document.getElementById("german").value);
+  const mathValue = Number(document.getElementById("math").value);
+
+  // 2. Das komplexe Schüler-Objekt zusammenbauen
+  const newStudent = {
+    firstName: firstNameValue,
+    lastName: lastNameValue,
+    age: ageValue,
+    schoolClass: classValue,
+    // Hier schachteln wir ein Objekt IN ein Objekt (für die Noten)
+    score: {
+      english: englishValue,
+      german: germanValue,
+      math: mathValue,
+    },
+  };
+
+  // 3. Das fertige Objekt in unser Array schieben
+  students.push(newStudent);
+
+  // Kleines Feedback für dich als Entwickler
+  console.log(firstNameValue + " wurde erfolgreich hinzugefügt!");
 }
 
-function sum() {
-  const sum = liste.reduce((previousValue, currentValue) => {
-    return previousValue + currentValue;
-  });
-  console.log(sum);
+function showStudents() {
+  // Gibt das gesamte Array mit allen Schülern in der Konsole aus
+  console.log(students);
 }
